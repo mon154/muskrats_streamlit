2b2aa4695f4e62f87031ab3090f3039e92c46f5e
Keyanna Duker <oli17003@byui.edu>
2022-06-29 21:00:36 -0600

    work page update

--- pages/work_page.py
+++ pages/work_page.py
@@ -13,16 +13,18 @@ df = get_data()
 
 # Write a header that says "Learning how to use Streamlit"
 
-
+st.header("Learning how to use Streamlit: ", anchor=None)
 
 # Create a check box labeled "Show data" that when checked, displays the dataframe
 
-
+agree = st.checkbox("Show Data", value=False, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
+if agree == True:
+    st.write(df)
 
 # Create a slider that allows the user to choose a range between two years from our movie dataset
-
-# start_color, end_color = st.select_slider(
-#      'Select a range of color wavelength',
-#      options=['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'],
-#      value=('red', 'blue'))
-# st.write('You selected wavelengths between', start_color, 'and', end_color)
+x = st.slider('Number of movies to display:', value=(1893,2005), min_value=1950, max_value=2010)
+#start_color, end_color = st.select_slider(
+#    'Select a range of movie years',
+#     options=['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'],
+#     value=('red', 'blue'))
+#st.write('You selected wavelengths between', start_color, 'and', end_color)
